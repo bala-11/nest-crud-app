6eea2e38a7b64c18b196b1da31860fcad8d1ddc4
bala-11 <balajikalimuthu482@gmail.com>
2023-03-28 09:41:43 +0530

    updated main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type : process.env.DB_TYPE as any,
+      host : process.env.PG_HOST,
+      port : parseInt (process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
